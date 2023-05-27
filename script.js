df891863cf30a8ee1375063a67a3400b9fee6a00
farrukhbek26 <fsakhatov@gmail.com>
2023-05-27 11:43:25 +0900

    JavaScript Array CRUD

---
+++ script.js
@@ -0,0 +1,47 @@
+let Cars = [ 
+        { id: 1, year: 1998, engine: 1, name: "Tico"},
+        { id: 2, year: 2005, engine: 1.2, name: "Matiz"},
+        { id: 3, year: 2010, engine: 1.6, name: "Gentra"},
+        { id: 4, year: 2010, engine: 1.5, name: "Cobalt"},
+        { id: 5, year: 2012, engine: 2, name: "Malibu"},
+        { id: 6, year: 2000, engine: 1.2, name: "Damas"},
+        { id: 7, year: 2018, engine: 2.4, name: "Tracker"}
+    ];
+// Sort
+// const sorts = Cars.sort((a ,b) => {
+//     return a.year - b.year;
+// })
+// console.log(sorts);
+
+// const sortName = Cars.sort((a, b) => {
+//     return a.name.localeCompare(b.name);
+// })
+// console.log(sortName)
+ 
+
+//Delete
+// const deleteUser = Cars.filter((value) => {
+//     return value.id == 1;
+// }) 
+// console.log(deleteUser);
+
+//Create
+// const addCar = (user) => {
+//    Cars = [...Cars, {id: Cars.length + 1, ...user}]
+// }
+// addCar({year: 1997, engine: 2.6, name: 'Ford'})
+// console.log(Cars);
+
+// const updateCar =(car) => {
+//    let res = Cars.map((value) => value.id === car.id ? {...value, name: 'Spark'} : value);
+//    console.log(res);
+
+// }
+
+// updateCar({id: 1})
+
+
+
+
+
+
